2eb2372d895d9b514b85391fb05bd0347a0f99ba
Amarnath K <amarnath_k@outlook.com>
2019-02-01 23:58:26 +0530

    Python App with GPU support

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
-FROM python:onbuild
+FROM terraopsio/cuda-python:cuda9.2-dev-python3
+ENV NUMBAPRO_NVVM=/usr/local/cuda-9.2/nvvm/lib64/libnvvm.so
+ENV NUMBAPRO_LIBDEVICE=/usr/local/cuda-9.2/nvvm/libdevice/
 ENV PORT 8080
 EXPOSE 8080
-ENTRYPOINT ["python"]
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY requirements.txt /usr/src/app/
+RUN pip3 install --quiet --no-cache-dir -r requirements.txt
+
+COPY . /usr/src/app
+ENTRYPOINT ["python3"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,24 +1,36 @@
-#!/usr/bin/python
+from flask import Flask
+import numpy as np
+from numba import vectorize
+from timeit import default_timer as timer
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+@vectorize(["float32(float32,float32)"], target='cuda')
+def Add(a, b):
+  return a + b
 
-PORT_NUMBER = 8080
+def CPU_Add(a,b,c):
+  for i in range(a.size):
+      c[i] = a[i] + b[i]
 
-class MyHandler(BaseHTTPRequestHandler):
+app = Flask(__name__)
 
-  def do_GET(self):
-    """Handler for GET requests"""
-    self.send_response(200)
-    self.send_header('Content-type','image/png')
-    self.end_headers()
-    with open('logo.png', 'rb') as f:
-      self.wfile.write(f.read())
 
-try:
-  server = HTTPServer(('', PORT_NUMBER), MyHandler)
-  print('Started httpserver on port', PORT_NUMBER)
-  server.serve_forever()
+@app.route('/')
+def hello():
+    N = 32000000
+    A = np.ones(N, dtype=np.float32)
+    B = np.ones(N, dtype=np.float32)
+    c = np.zeros(N, dtype=np.float32)
+    d = np.zeros(N, dtype=np.float32)
+    start = timer()
+    c = Add(A, B)
+    duration = timer() - start
+    cpustart = timer()
+    CPU_Add(A, B, d)
+    duration2 = timer() - cpustart
+    print(duration)
+    print("c[:5] = " + str(c[:5]))
+    print("c[-5:] = " + str(c[-5:]))
+    return "App Version: 1 <BR> GPU Time: " + str(duration) + "<BR> CPU Time: " +  str(duration2)
 
-except KeyboardInterrupt:
-  server.server_close()
-  print('Stopping server')
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=8080)
